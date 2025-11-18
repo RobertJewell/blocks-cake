7c3d7abb736fafe12ee18cea39e9eb07d05d3855
RobertJewell <rjewellaudio@gmail.com>
2025-11-18 07:17:13 +0100

    avoid error on missing user

--- src/routes/app/route.tsx
+++ src/routes/app/route.tsx
@@ -4,12 +4,15 @@ import { createFileRoute, Outlet } from "@tanstack/react-router";
 export const Route = createFileRoute("/app")({
   component: Outlet,
   beforeLoad: async ({ context }) => {
-    const user = await context.queryClient.ensureQueryData({
-      ...authQueryOptions(),
-      revalidateIfStale: true,
-    });
+    try {
+      const user = await context.queryClient.ensureQueryData({
+        ...authQueryOptions(),
+        revalidateIfStale: true,
+      });
 
-    // re-return to update type as non-null for child routes
-    return { user };
+      return { user };
+    } catch (err) {
+      return { user: null };
+    }
   },
 });
