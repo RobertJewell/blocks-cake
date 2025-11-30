6bddde4050f3ad3df69e4b23e37ccdfddaefdeab
RobertJewell <rjewellaudio@gmail.com>
2025-11-30 20:14:36 +0100

    add url to asset type

--- src/lib/cms/blocks/block-builder.ts
+++ src/lib/cms/blocks/block-builder.ts
@@ -4,6 +4,7 @@ import React from "react";
 export const assetSchema = z.object({
   id: z.string(),
   filename: z.string().optional(),
+  url: z.string().optional(),
   alt: z.string().nullable().optional(),
   blurhash: z.string().nullable().optional(),
   width: z.number().default(0),
